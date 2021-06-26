29377cc226103b6abaa018e2eb5e6d14756925de
Lais Pereira <lais.pereira.santos92@gmail.com>
2021-06-26 00:33:35 -0300

    alterando o footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Menu from '../../components/Menu'
-import Banner from '../../components/Banner'
-
+import React from "react";
+import Menu from "../../components/Menu";
+import Banner from "../../components/Banner";
+import Footer from "../../components/Footer";
 const Home = () => {
-    return(
-        <>
-            <Menu />
-            <Banner />
-        </>
-    )
-}
+  return (
+    <>
+      <Menu />
+      <Banner />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
