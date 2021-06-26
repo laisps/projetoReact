f25764487ec6aa4ded296d96c5ef8e542369a55e
Lais Pereira <lais.pereira.santos92@gmail.com>
2021-06-25 21:47:27 -0300

    Adicionando pagina github

--- src/pages/GitHub/index.js
+++ src/pages/GitHub/index.js
@@ -1,24 +1,51 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+import Menu from "../../components/Menu";
+import "./styles.css";
 
-const Segundo = () => {
-    const [repositorios, setRepositorios] = useState([])
+function GitHub() {
+  const [repositories, setRepositories] = useState([]);
+  const [repositoreFilter, setFilter] = useState([]);
+  const [search, setsearch] = useState("");
 
-    useEffect(()=>{
-        const pegaDados = async () => {
-            const resposta = await fetch('https://api.github.com/users/simaraconceicao/repos')
-            const dados = await resposta.json()
-            setRepositorios(dados)
-        }
-        pegaDados()
-    },[])
+  useEffect(() => {
+    async function Dados() {
+      const response = await fetch("https://api.github.com/users/laisps/repos");
+      const data = await response.json();
 
-    return(
-        <ul>
-            {repositorios.map(repo => (
-                <li key={repo.id}>{repo.name}</li>
-            ))}
-        </ul>
-    )
+      setRepositories(data);
+    }
+    Dados();
+  }, []);
+
+  useEffect(() => {
+    setFilter(
+      repositories.filter((repo) => {
+        return repo.name.includes(search);
+      })
+    );
+  }, [search, repositories]);
+
+  return (
+    <div>
+      <Menu />
+      <input
+        type="text"
+        placeholder="Digite o nome do repositorio que deseja encontrar"
+        onChange={(e) => {
+          setsearch(e.target.value.toLowerCase());
+        }}
+      />
+      <ul className="repos">
+        {repositoreFilter.map((repo) => {
+          return (
+            <li className="repositores" key={repo.id}>
+              {repo.name}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default Segundo
+export default GitHub;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from '../pages/Home'
-import Simpsons from '../pages/Simpsons'
+import Home from "../pages/Home";
+import Simpsons from "../pages/Simpsons";
+import GitHub from "../pages/Github";
 
 const Routes = () => {
-    return(
-        <BrowserRouter>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route path='/simpsons' component={Simpsons} />
-            </Switch>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/github" component={GitHub} />
+        <Route path="/simpsons" component={Simpsons} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default Routes
+export default Routes;
